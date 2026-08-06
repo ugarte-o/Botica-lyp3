a438137eedc15b23ebfbc32291d2a1e6670ab771
ugarte-o <obeduga123@gmail.com>
2026-08-05 19:09:38 -0500

    Update database configuration in db.php

--- src/app/cfg/db.php
+++ src/app/cfg/db.php
@@ -1,9 +1,11 @@
 <?php
-
-$data = array(
-    "host" => "127.0.0.1",
-    "db"   => "botica",
-    "user" => "root",
-    "pass" => "",
-    "port" => "3306",
+$data=array(
+	"host"=>"localhost",
+	"db"=>"meralda",
+	"user"=>"meralda",
+	"pass"=>"",
+	"port"=>"3306",
+	
 );
+
+?>
